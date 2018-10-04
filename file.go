04dfcb30f1531aa6d5370fe09150eed28dd134ef
taoyu <1255803549@qq.com>
2018-10-04 15:50:15 +0800

    extra

--- file.go
+++ file.go
@@ -7,7 +7,7 @@ type FileModel struct {
 	Format    string    `json:"format" gorm:"not null"`
 	Filename  string    `json:"filename" gorm:"not null"`
 	Size      int64     `gorm:"not null"`
-	Extra     string
+	Extra     string    `gorm:"not null;type:text"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
